5cba817bf777132586db79adec6767724757bb20
CameronParker1 <par18060@byui.edu>
2023-11-07 14:03:51 -0700

    Finished Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue", 8);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Orange", 3, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Purple", 4);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
